22c547a73b9e1e3d15c246a26b15c40a43480c10
Waleed <masterwali0402@gmail.com>
2022-02-23 07:06:34 -0500

    Minor improvements

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-terraform {
-  backend "s3" {
-    key = "tf-partial-backend-example/terraform-main.tfstate"
-  }
-}
-
 resource "aws_ssm_parameter" "this" {
   name  = "example-param-${var.environment}"
   type  = "String"
--- versions.tf
+++ versions.tf
@@ -1,3 +1,14 @@
 terraform {
   required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.2.0"
+    }
+  }
+
+  backend "s3" {
+    key = "tf-partial-backend-example/terraform-main.tfstate"
+  }
 }
